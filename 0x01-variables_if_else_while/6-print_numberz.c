ebcae661c0f3e7889b43fe970a8b3bd26906f058
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-09 16:56:30 -0700

    God Help me this command not to return an error

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- *
  * main - entry point
  *
  * Return: alway returns 0
@@ -11,6 +8,7 @@
 int main(void)
 {
 	int a = 0;
+
 	for (; a < 10 ; a++)
 		putchar('0' + a);
 	putchar('\n');
