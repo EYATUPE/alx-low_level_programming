61ca3c2924cc139770cab1f11fef271531951baa
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-09 05:34:40 -0700

    Hello C Programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 
 /**
- *  * main - This code prints the text
- *   *
- *    * Return: always 0 (And is't successful)
- *     */
+ * main - This code prints the text
+ *
+ * Return: always 0 (And is't successful)
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
