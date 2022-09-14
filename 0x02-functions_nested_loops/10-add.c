c7c0e38b6cc705a599b8a547b2148d243b27a6d6
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-14 13:55:41 -0700

    This Errors can kill someone ehh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @num3: The first integer to be added.
+ * @num4: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int num3, int num4)
+{
+	return (num3 + num4);
+}
