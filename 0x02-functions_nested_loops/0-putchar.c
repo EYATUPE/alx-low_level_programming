1ac4dab7d9976b662ec46ff61e732331f5ef6551
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-14 11:48:33 -0700

    Trial 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  
  int main(void)
  {
-	 putchar('');
+ 	putchar('_');
 	 _putchar('p');
 	 _putchar('u');
 	 _putchar('t');
@@ -16,6 +16,6 @@
 	 _putchar('a');
 	 _putchar('r');
 
- return (0);
+	 return (0);
 
 }
