f0e86b0ef46f737ee9267bdf745e3f376739c4ba
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-16 14:14:01 -0700

    God Please help Me

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints the numbers from 1 to 100, But for multiples of 3
  * or for multiples of five to print fizzBuzz
  *
- * Return; returns 0
+ * Return: returns 0 (ALX please help me pass)
  */
 int main(void)
 {
 	int i;
 
-	for (i =1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i == 100)
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (((i % 3) == 0) && ((i % 5) == 0))
-			printf("FizzBuzz ");
-		else if ((i % 3) == 0)
-			printf("Fizz ");
-		else if ((i % 5) == 0)
-			printf("Buzz ");
 		else
-			printf("%d ", i);
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
