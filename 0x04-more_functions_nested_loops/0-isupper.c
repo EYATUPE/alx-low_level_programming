a89ee2a1e54e9ff25659b3d8e51f7a0c8741c84d
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-16 12:33:03 -0700

    God help me

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_isupper - checks if uppercase
  *@c: character to check
@@ -6,10 +7,10 @@
  *Return: 1 or 0
  */
 int _isupper(int c)
+
 {
-	if(c >='A' && c <='Z')
-	{
+	if(c >='A' && c <= 'Z')
 		return (1);
-	}
+	else
 		return (0);
 }
