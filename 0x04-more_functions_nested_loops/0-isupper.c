581e1e6b15c323e65baaec123c62b5ac4e4c2c48
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-16 03:39:42 -0700

    I thank You God

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- *_isupper -Uppercase of not
+ *_isupper - uppercase of not
  *@c: character to be debugged
- *Return: Either 1 or 0
+ *
+ *Return: either 1 or 0
  */
 int _isupper(int c);
 {
-	if (c >= 'A') && (c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
