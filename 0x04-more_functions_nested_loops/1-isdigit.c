2269874905e51b294024528047d679f6980c79e7
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-16 14:54:32 -0700

    God Please help Me

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,11 @@
 /**
  * _isdigit -checks whether a character is a digit or not
  * @c: character check
- * Return: 1 if it is, 0 otherwise
- */
+ * Return: 1 or  0
+ **/
 int _isdigit(int c)
 {
-	if ((c >= '48') && (c <= '57'))
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}
