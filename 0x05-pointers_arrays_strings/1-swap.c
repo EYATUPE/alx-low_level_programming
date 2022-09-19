de7f119d00e3795b98da99983c567bcf2de20de2
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-19 08:45:08 -0700

    C Programming is not hard

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *swap_int - swap two integers
+ *@a: first integer
+ *@b: second integer
+ *Return : Always (Esamu does his own work)
+ */
+void swap_int(int *a, int *b)
+{
+	int age = 26;
+	int* ptr = &age;
+	*ptr = 27;
+}
