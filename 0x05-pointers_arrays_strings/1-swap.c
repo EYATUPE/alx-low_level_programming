8d9bb8d5244a70017751519056b1825a3578b220
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-19 09:29:01 -0700

    Esamu does his work by hustle

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,12 @@
 #include "main.h"
-/*
- * swap int - swaps two integers' values
- * @a:first integer
- * @b:second integer
- * Return: returns nothing
+
+/**
+ *swap_int - swaps two integers' values
+ *@a:first integer
+ *@b:second integer
+ *Return: returns (Esamu struggles to debugg his Tasks)
  */
+
 void swap_int(int *a, int *b)
 {
 	int php = *a;
