028c4f4e6de2a97076a91679742c7c3fc4382a08
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-19 08:56:20 -0700

    Esamu does his work by hustle

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /*
  *swap_int - swaps two integers' values
  *@a: first integer
  *@b: second integer
  *Return: Always (Esamu does his own work)
  */
-
 void swap_int(int *a, int *b)
 {
 	int age = *a;
