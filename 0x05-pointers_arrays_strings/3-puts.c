135d029e149e9f9fc16a4bb8ccabb373baebf647
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-19 10:47:44 -0700

    Today the network is unfriendly

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,6 @@
 /**
  * _puts - prints a string, followed by a new line
  * @str: string to be printed
- * 
  * Description: prints a string
  * on success: returns no error
  */
@@ -14,9 +13,9 @@ void _puts(char *str)
 	int e = 0;
 
 	while (*(str + e) != '\0')
-		{
-			putchar(*(str + e));
-			e++;
-		}
+	{
+		putchar(*(str + e));
+		e++;
+	}
 	putchar(10);
 }
