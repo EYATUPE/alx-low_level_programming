3b7aa5a86209a6fd4ea59704927a378f14f20ccc
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-20 13:00:36 -0700

    Super User Esamu

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,7 +3,8 @@
 
 /**
  *_atoi: converts a string to an integer
- *@str: iii
+ *@s: String to be converted
+ *Return: converted integer
  */
 
 int _atoi(char *s)
@@ -19,6 +20,7 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -34,11 +36,11 @@ int _atoi(char *s)
 			if (s[i + 1] < '0' || s[i + 1] > 9)
 				break;
 			f = 0;
-			i++;
-		}
-		if (f == 0)
-			return (0);
 
-		return (n);
+		}
+		i++;
 	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
