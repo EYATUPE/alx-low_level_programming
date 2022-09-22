6849def3877e443e42ce8d4f1b2d9564ab0c6e07
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-22 02:47:52 -0700

    Friday is the Day

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,8 +12,9 @@ int _strcmp(char *s1, char *s2)
 	int e = 0, hp;
 
 	while (hp == 0)
+
 	{
-		if ((*(s1 + hp) == '\0') && (*(s2 + e) == '\0'))
+		if ((*(s1 + e) == '\0') && (*(s2 + e) == '\0'))
 			break;
 		hp = *(s1 + e) - *(s2 + e);
 		e++;
