82219ebee22d6564182b0c52229589a911e69408
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-22 02:41:00 -0700

    Friday is the Day

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *string_toupper - Changing lower cases to upper cases
+ *Return: Capitalized characters
+ */
+
+char *string_toupper(char *)
+{
+	char str[] = "eyatu philip esamu\n";
+	char ch;
+
+	putchar(toupper(ch));
+	return (0);
+}
