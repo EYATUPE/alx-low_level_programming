394dda0284dea777a4991392100364865da34109
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-23 08:32:24 -0700

    Hard things

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *cap_string - Capitalizes all words of a string.
+ *@p: String to  be capitalized
+ *Return: Code word Capital City
+ */
+
+char *cap_string(char *p)
+{
+	p = "esamu,philip;eyatu"
+		char str[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}',};
+	int len = 13;
+	int a = 0, i;
+
+	while (p[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || e[a - 1] == spc[i]) && (e[a] >= 97 && e[a] <= 122))
+				e[a] = e[a]  - 32;
+			i++;
+		}
+		a++;
+	}
+	return (p);
+}
