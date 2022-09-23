5907c1e484f44d817da606ab9b887f950d37cc71
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-23 08:52:44 -0700

    Hard Things

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,9 @@
 
 char *cap_string(char *p)
 {
-	p = "esamu,philip;eyatu"
+	p = "esamu philip eyatu"
 		char str[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}',};
+
 	int len = 13;
 	int a = 0, i;
 
@@ -18,7 +19,7 @@ char *cap_string(char *p)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || e[a - 1] == spc[i]) && (e[a] >= 97 && e[a] <= 122))
+			if ((a == 0 || e[a - 1] == str[i]) && (e[a] >= 97 && e[a] <= 122))
 				e[a] = e[a]  - 32;
 			i++;
 		}
