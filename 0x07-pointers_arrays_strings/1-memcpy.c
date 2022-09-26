07dca19dea7a6f5b41c1520ba3b30b9717a8cbd9
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-26 13:55:28 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: where the memory area is to be pasted
+ *@scr: Where the memory area is to be copied from
+ *@n: the number of the bytes to be copied
+ *
+ *Returns - a pointer destination
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int e = 0;
+
+	for (; e < n; e++)
+	{
+		dest[e] = src[i];
+	}
+	return (dest);
+}
