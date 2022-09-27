6e7a153bc45d11cdaad634dcbd11c37f70fe4a9b
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-27 04:39:46 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,13 +3,13 @@
 /**
  *_strspn - function that gets the length of a prefix substring
  *@s: - character of the pointer
- *@a: - accept pointer
+ *@accept: - accept pointer
  *Return - the number of bytes in the initial segment of s
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < accept)
 	{
