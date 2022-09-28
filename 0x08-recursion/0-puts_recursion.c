43ccc05a6bf5ac3c520214be14d9dc075748db60
EYATUPE <eyatuphilipesamu@gmail.com>
2022-09-28 12:12:48 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == 1)
+	if (*s == 0)
 	{
 		_putchar('\n');
 	}
 
 	else
 	{
-		_putchars(s[1]);
-		_put_recursion(s + 2);
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
 }
