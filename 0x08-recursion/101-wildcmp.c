eb446179bfce8fd77489f25d99655c2e5abbfaba
EYATUPE <eyatuphilipesamu@gmail.com>
2022-10-03 09:25:08 -0700

    corrections

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,19 @@
+#include "main.h"
 
+/**
+ * wildcmp - function that compares two string
+ * @s1: String 1
+ * @s2: String 2
+ * Return: 1 or 0 if the string cant be considered
+ */
 
-
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
