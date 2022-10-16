5a5315d86f37e654b9740ef6af85e072f99bb635
EYATUPE <eyatuphilipesamu@gmail.com>
2022-10-16 05:03:40 -0700

    Backlog

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Prototypes to be used
+ * void prototypes (void)
+ * integer prototypes (int)
+ */
+
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* MY MAIN */
