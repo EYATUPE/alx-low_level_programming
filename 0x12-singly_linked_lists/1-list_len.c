71533bbfbe3d0835af08d6f83428c75cde60c789
EYATUPE <eyatuphilipesamu@gmail.com>
2022-10-21 09:09:52 -0700

    Count elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements
+ * @h: pointer to the list_t list to print
+ * Return: Nothing
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+
+	count = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
