26216ba7bb18a2bd4d2a51c5340114051b76ecd5
EYATUPE <eyatuphilipesamu@gmail.com>
2022-10-24 07:56:36 -0700

    Failing still

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _listint - a function that prints all the elements of a listint_t list.
+ * @h: The node to be used
+ * Return: Returning the number of nodes
+ * Task 0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *tmp;
+	unsigned int e;
+
+	tmp = h;
+	for (e = 0; tmp; e++)
+	{
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+	}
+	return (e);
+}
